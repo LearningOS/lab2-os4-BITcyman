ffe574431172f0f9d941dc96e53b0daa9d39402a
BITcyman <815207911@qq.com>
2023-02-23 13:26:15 +0000

    update

--- os4/src/mm/memory_set.rs
+++ os4/src/mm/memory_set.rs
@@ -3,8 +3,13 @@ use super::{VirtPageNum, FrameTracker, VPNRange};
 
 
 pub struct MapArea {
-    vpn_range: VPNRange，
+    vpn_range: VPNRange, 
     data_frame: BTreeMap<VirtPageNum, FrameTracker>,
     map_type: MapType,
     map_perm: MapPermission,
+}
+
+pub enum MapType {
+    Identical,  //  恒等映射
+    Framed,     // 新分配一个物理页帧与之对应
 }
